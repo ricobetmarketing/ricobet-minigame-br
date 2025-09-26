91b9db8c621665557c5de72669367ea3957506f8
ricobetmarketing <marketing@rico.bet>
2025-09-26 17:20:20 +0800

    Update common.js

--- assets/common.js
+++ assets/common.js
@@ -1,13 +1,12 @@
-<script> 
+// assets/common.js
 window.MG_LOCK_KEY = 'mg_game_choice_v1';
 
 window.getChoice = () => localStorage.getItem(window.MG_LOCK_KEY);
 window.setChoice = (choice) => localStorage.setItem(window.MG_LOCK_KEY, choice);
 window.clearChoice = () => localStorage.removeItem(window.MG_LOCK_KEY);
 
-// Optional test reset via query (?reset=1)
+// Optional: allow reset via URL param (?reset=1)
 (() => {
   const p = new URLSearchParams(location.search);
   if (p.get('reset') === '1') localStorage.removeItem(window.MG_LOCK_KEY);
 })();
-</script>
